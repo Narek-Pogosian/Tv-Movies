9f9db769278bbfaea39c938513293c6d5960b2f4
Narek Pogosian <narekpogosian2@gmail.com>
2024-03-10 18:07:16 +0100

    change back dialog compo to headless ui

--- src/pages/(details)/components/trailer-modal.tsx
+++ src/pages/(details)/components/trailer-modal.tsx
@@ -1,26 +1,58 @@
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import { Video } from "@/lib/types";
 import { Button } from "@/components/ui/button";
+import { Video } from "@/lib/types";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
 
-function TrailerModal({ trailer }: { trailer: Video }) {
+const TestModal = ({ trailer }: { trailer: Video }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="gradient">
-          <img src="/play.svg" alt="" />
-          <span>Play Trailer</span>
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="px-0 overflow-hidden max-w-4xl py-10 h-[300px] sm:h-[400px] lg:h-[500px]">
-        <iframe
-          className="z-50 w-full h-full"
-          src={`https://www.youtube-nocookie.com/embed/${trailer.key}`}
-          allow="autoplay; encrypted-media"
-          allowFullScreen
-        ></iframe>
-      </DialogContent>
-    </Dialog>
-  );
-}
+    <>
+      <Button variant="gradient" onClick={openModal}>
+        <img src="/play.svg" alt="" />
+        <span>Play Trailer</span>
+      </Button>
 
-export default TrailerModal;
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-50" onClose={closeModal}>
+          <div className="fixed inset-0 bg-black/60 backdrop-blur-sm" />
+          <div className="fixed inset-0">
+            <div className="flex items-center justify-center min-h-full p-4 text-center">
+              <Dialog.Panel className="w-full relative max-w-4xl rounded-lg bg-element border py-10 h-[300px] sm:h-[400px] lg:h-[500px] overflow-hidden text-left align-middle transition-all transform bg-black shadow-xl">
+                <button
+                  className="absolute text-xl top-1 right-2"
+                  aria-label="close"
+                  onClick={closeModal}
+                >
+                  &#x2715;
+                </button>
+                <Trailer trailer={trailer} />
+              </Dialog.Panel>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+};
+
+export default TestModal;
+
+const Trailer = ({ trailer }: { trailer: Video }) => {
+  return (
+    <iframe
+      className="z-50 w-full h-full"
+      src={`https://www.youtube-nocookie.com/embed/${trailer.key}`}
+      allow="autoplay; encrypted-media"
+      allowFullScreen
+    ></iframe>
+  );
+};
--- src/pages/(discover)/components/discover-layout.tsx
+++ src/pages/(discover)/components/discover-layout.tsx
@@ -1,9 +1,12 @@
+import Filters from "./filters/filters";
 import ScrollTopButton from "./scroll-top-button";
 
 function DiscoverLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col gap-8 py-4 xl:flex-row">
-      <div className="w-full xl:w-fit xl:sticky top-24 h-fit">FILTERS</div>
+      <div className="w-full xl:w-fit xl:sticky top-24 h-fit">
+        <Filters />
+      </div>
       {children}
       {<ScrollTopButton />}
     </div>
