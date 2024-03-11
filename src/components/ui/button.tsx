3d551c1a3847e44c1b5a1590abf3580fda912be4
Narek Pogosian <narekpogosian2@gmail.com>
2024-03-11 19:46:54 +0100

    edit button shadow

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
       variant: {
         default:
           "bg-primary text-primary-foreground shadow hover:bg-primary/90",
-        gradient: "primary-gradient text-white shadow-md dark:shadow-lg",
+        gradient: "primary-gradient text-white shadow-md dark:shadow-lg dark:shadow-black/70",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
